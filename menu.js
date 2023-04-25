e1bbb5f215ef6cfaa91bbf4c1c8fbb7e8abe4cdd
oldtagz <kiry.loetscher@gmail.com>
2023-04-25 17:40:18 +0700

    Style checkout page correctly with animation

--- menu.js
+++ menu.js
@@ -1,7 +1,7 @@
 const MENU_HTML = `
 <div id="menu" class="slide-in" onshow="this.classList = ''">
   <button
-    onclick="document.getElementById('menu').classList = 'slide-out'; setTimeout(() => document.body.removeChild(document.getElementById('menu')), 250)"
+    onclick="closeMenu()"
     id="menu-close-button"
     class="close"
   >
@@ -22,4 +22,19 @@ const MENU_HTML = `
     </a>
   </div>
 </div>
-`
+`;
+
+const openMenu = () => {
+  document.getElementById("menu-button").style.opacity = "0.1";
+  document.body.style.overflowY = "hidden";
+  document.body.innerHTML += MENU_HTML;
+  setTimeout(() => (document.getElementById("menu").classList = ""), 250);
+};
+
+const closeMenu = () => {
+  document.getElementById("menu-button").style.opacity = "1";
+  const menu = document.getElementById("menu");
+  menu.classList = "slide-out";
+  document.body.style.overflowY = "scroll";
+  setTimeout(() => document.body.removeChild(menu), 250);
+};
