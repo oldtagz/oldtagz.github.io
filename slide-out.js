9021e6c35a217b59dad30088930aefcfb5391a47
oldtagz <kiry.loetscher@gmail.com>
2023-04-27 16:10:41 +0700

    Optimize menu & close buttons

--- slide-out.js
+++ slide-out.js
@@ -1,12 +1,17 @@
 const SLIDE_OUT_IDS = ["menu", "checkout"];
-const LISTENERS = SLIDE_OUT_IDS.reduce((acc, id) => ({
-  ...acc,
-  [id]: () => closeSlideOut(id),
-}), {});
+const LISTENERS = SLIDE_OUT_IDS.reduce(
+  (acc, id) => ({
+    ...acc,
+    [id]: () => closeSlideOut(id),
+  }),
+  {}
+);
 
 const openSlideOut = (id) => {
+  console.log("OK");
   document.getElementById(id).classList = "open slide-in";
-  document.getElementById(`${id}-button`).style.opacity = "0";
+  if (id === "menu")
+    document.getElementById(`${id}-button`).classList += " close";
   document.body.style.overflowY = "hidden";
   setTimeout(() => {
     document.body.addEventListener("click", LISTENERS[id]);
@@ -18,10 +23,10 @@ const openSlideOut = (id) => {
 
 const closeSlideOut = (id) => {
   document.body.removeEventListener("click", LISTENERS[id]);
+  if (id === "menu")
+    document.getElementById(`${id}-button`).classList = id;
   const element = document.getElementById(id);
   document.getElementById(id).classList = "open slide-out";
   setTimeout(() => (element.classList = ""), 250);
-
-  document.getElementById(`${id}-button`).style.opacity = "1";
   document.body.style.overflowY = "scroll";
 };
